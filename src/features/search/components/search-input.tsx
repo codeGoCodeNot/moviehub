edfabac6ee40813db02bff84f167f9f0fc04830d
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-05-12 18:17:45 +0800

    use navigate added

--- src/features/search/components/search-input.tsx
+++ src/features/search/components/search-input.tsx
@@ -3,11 +3,18 @@ import { LucideSearch, LucideX } from "lucide-react";
 import { useDebouncedCallback } from "use-debounce";
 import useEndpointStore from "@/state-management/stores/useEndPointStore";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const searchQuery = useEndpointStore((s) => s.searchQuery);
   const setSearchQuery = useEndpointStore((s) => s.setSearchQuery);
   const [localValue, setLocalValue] = useState(searchQuery);
+
+  const navigate = useNavigate();
+
+  const handleHome = () => {
+    navigate("/");
+  };
 
   const debounced = useDebouncedCallback((value: string) => {
     setSearchQuery(value);
@@ -25,7 +32,7 @@ const SearchInput = () => {
   };
 
   return (
-    <div className="w-2/3 relative mr-10">
+    <div className="w-2/3 relative mr-10" onClick={handleHome}>
       <LucideSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground w-4 h-4 pointer-events-none" />
       <Input
         placeholder="Search movies, shows, people..."
